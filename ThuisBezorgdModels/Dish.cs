e50c414e78f2e41e40c7133abdc1758a3b20a28b
Jildert <jildert.venema@hotmail.com>
2021-02-27 15:34:14 +0100

    Initial project

---
+++ ThuisBezorgdModels/Dish.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ThuisBezorgdModels
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public decimal ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
